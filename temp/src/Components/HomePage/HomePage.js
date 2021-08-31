8975ad14a5807ebda1e473ced3aaf531c7b4282f
Paul Shorkey-Chacon <paul@pauls-mbp.lan>
2021-08-31 12:19:20 -0400

    First commit

---
+++ temp/src/Components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/// FILE IMPORTS ///
+import AppBar from './AppBar/AppBar.js'
+
+
+export default function Home(){
+    
+    return(
+      <div>
+        <AppBar />
+      </div>
+        
+    );
+}
